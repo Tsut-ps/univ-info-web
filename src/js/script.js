172fe71543b372d498792e1e0353379e7b8efd84
Tsut-ps <haru.pichan.kiro@gmail.com>
2023-07-12 15:55:03 +0900

    アニメーション実装

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,23 @@
+// テストアプリケーション
 Vue.createApp({
 	data() {
 		return {
-			messageraw: "あああ@ああ"
+			messageraw: "あああ@ああ",
+			name: "いいいいい"
 		};
 	},
 	computed: {
 		message() {
 			return this.messageraw.split('@')[0];
 		}
+	},
+	components: {
+		'my-component': {
+			template: '<p>aaa{{ name }}</p>'
+		}
 	}
 }).mount('#app');
+
+// メインビジュアルアプリケーション
+Vue.createApp({
+}).mount('#visual-app');
