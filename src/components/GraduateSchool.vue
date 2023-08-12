07a855c224181f63a3de62feb5ee32f2e69d079e
Tsut-ps <haru.pichan.kiro@gmail.com>
2023-08-13 01:04:42 +0900

    背景画像が表示されないバグを修正

--- src/components/GraduateSchool.vue
+++ src/components/GraduateSchool.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="sub-visual" class="mt-5 py-5">
+    <div id="sub-visual" class="mt-5 py-5" :style="`background-image: url(${backVisualImg})`">
         <!-- コンテナ / フレックス要素で中心揃え / 内側に余白 / 縁を丸くする / ちょっと動くアニメーション / はみ出た部分は非表示(画像) -->
         <div class="container d-flex align-items-center p-5 rounded move overflow-hidden">
             <div class="fit-content me-3">
@@ -8,17 +8,21 @@
             </div>
             <span class="right-arrow"></span>
             <!-- 画像を表示 / 画面幅が広いときのみ表示 -->
-            <img src="../img/krisztian-tabori-IyaNci0CyRk-unsplash.jpg" alt="大学院アイキャッチ" id="mini-visual"
+            <img :src="miniVisualImg" alt="大学院アイキャッチ" id="mini-visual"
                 class="d-none d-md-inline-block">
         </div>
     </div>
 </template>
 
+<script setup lang="ts">
+import miniVisualImg from '../img/krisztian-tabori-IyaNci0CyRk-unsplash.jpg'
+import backVisualImg from '../img/marvin-meyer-SYTO3xs06fU-unsplash.jpg'
+</script>
+
 <style scoped>
 /* サブビジュアル */
 #sub-visual {
     width: 100%;
-    background-image: url('../src/img/marvin-meyer-SYTO3xs06fU-unsplash.jpg');
     background-size: cover;
 }
 #sub-visual .container {
