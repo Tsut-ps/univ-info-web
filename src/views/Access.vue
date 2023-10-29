3b069217525288a8c58ed59f58b93a41d0187c5e
Tsut-ps <haru.pichan.kiro@gmail.com>
2023-10-29 16:32:19 +0900

    allowfullscreenが未指定なために出るビルド時のエラーを修正

--- src/views/Access.vue
+++ src/views/Access.vue
@@ -15,7 +15,7 @@
                 <p>三軒茶屋駅から徒歩5分</p>
                 <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1146.3903396129883!2d139.66842517030227!3d35.64050606011255!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6018f49927231ef9%3A0xcc2931a04ac73fe6!2z44CSMTU0LTAwMjQg5p2x5Lqs6YO95LiW55Sw6LC35Yy65LiJ6LuS6Iy25bGL!5e0!3m2!1sja!2sjp!4v1689380465728!5m2!1sja!2sjp"
-                    width="100%" height="480px" style="border:0;" allowfullscreen="" loading="lazy"
+                    width="100%" height="480px" style="border:0;" loading="lazy"
                     referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </article>
