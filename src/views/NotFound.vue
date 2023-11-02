fcc9a461d86d51ea90e2ad0d4b6d7fa2938cd442
Tsut-ps <haru.pichan.kiro@gmail.com>
2023-11-02 20:06:12 +0900

    404ページをrouterで実装

---
+++ src/views/NotFound.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import router from '@/router'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+// 5秒後にトップページを表示する
+onMounted(() => {
+    setTimeout(() => {
+        // 今開いているページがトップページなら何もしない
+        if (route.path === '/') return
+        router.push('/')
+    }, 5000)
+})
+</script>
+
+
+<template>
+  <main>
+
+    <!-- 本文の要素 / 上部に外部余白 -->
+    <article class="mt-5">
+        <!-- コンテナを用意(横に余白) -->
+        <div class="container">
+            <!-- 横並びにする -->
+            <div class="row">
+                <!-- ヘッダー上にサブタイトルを表示 -->
+                <span class="subtitle">Not Found</span>
+                <h2>お探しのページは見つかりませんでした。</h2>
+                <p>5秒後にトップページへ戻ります。</p>
+            </div>
+        </div>
+    </article>
+
+  </main>
+</template>
