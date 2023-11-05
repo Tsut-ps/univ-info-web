67b5b2390dad1ea819bb4a37f3e641549e782b3f
Tsut-ps <haru.pichan.kiro@gmail.com>
2023-11-06 02:10:47 +0900

    GitHubへのリンクを作成

--- src/components/Copyright.vue
+++ src/components/Copyright.vue
@@ -5,7 +5,10 @@
         <div class="container-fluid py-5 bg-light">
             <!-- フレックス要素にして中央揃え -->
             <div class="d-flex justify-content-center">
-                <span>Copyright © Tokyo Information Design University.</span>
+                <span class="m-1">
+                    Copyright © Tokyo Information Design University.
+                    <a href="https://github.com/Tsut-ps/univ-info-web" class="m-1">GitHub</a>
+                </span>
             </div>
         </div>
     </footer>
