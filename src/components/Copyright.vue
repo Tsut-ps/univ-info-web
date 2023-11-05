6de11bbb07d98823729d7b80cf1836d029629d97
Tsut-ps <haru.pichan.kiro@gmail.com>
2023-11-06 02:31:42 +0900

    Copyrightの要素内もセンタリングするように

--- src/components/Copyright.vue
+++ src/components/Copyright.vue
@@ -3,8 +3,8 @@
     <footer>
         <!-- 著作情報 -->
         <div class="container-fluid py-5 bg-light">
-            <!-- フレックス要素にして中央揃え -->
-            <div class="d-flex justify-content-center">
+            <!-- フレックス要素にして中央揃え / 要素の文章を中央揃え -->
+            <div class="d-flex justify-content-center text-center">
                 <span class="m-1">
                     Copyright © Tokyo Information Design University.
                     <a href="https://github.com/Tsut-ps/univ-info-web" class="m-1">GitHub</a>
